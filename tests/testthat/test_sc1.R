93510ed70bb5ec689cd2ac32c86824c9d04cd0ec
scano <Stephane.Cano@pmi.com>
2020-11-12 18:07:50 +0100

    Fix "function not found" in some tests

--- tests/testthat/test_sc1.R
+++ tests/testthat/test_sc1.R
@@ -1,4 +1,4 @@
 test_that("sc1:returns pval.zero among the list of sc1 functions", {
-    expect_true("pval.zero"%in%names(sc1_mthds()))
+    expect_true("pval.zero"%in%names(GladiaTOX:::sc1_mthds()))
 })
 
--- tests/testthat/test_sc2.R
+++ tests/testthat/test_sc2.R
@@ -1,4 +1,4 @@
 test_that("sc2:returns pval.zero among the list of sc2 functions", {
-    expect_true("bmad6"%in%names(sc2_mthds()))
+    expect_true("bmad6"%in%names(GladiaTOX:::sc2_mthds()))
 })
 
